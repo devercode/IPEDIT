7e24bc43df153ba1eb53369bbae27e8e71c29e6c
thinhpham@play-force.com.au <thinhpham@play-force.com.au>
2021-12-12 02:03:19 +0700

    modal login

---
+++ pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Login />
+</template>
+
+<script>
+// import Main from "../components/main.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  // components: {
+  //   Main
+  // }
+};
+</script>
