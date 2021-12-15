efd5c94a4eef5d012d8e4e508dea1323b9277046
thinhpham@play-force.com.au <thinhpham@play-force.com.au>
2021-12-15 15:03:26 +0700

    change version

---
+++ components/UseFree.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="useFree">
+    <div class="useFree__container container">
+      <h1>지금 바로 무료 체험판 이용을 시작하세요!</h1>
+      <a class="button btn btn-useFree" v-on:click="openModalSignUp">
+        IPEDIT 시작하기
+      </a>
+    </div>
+    <Login
+      v-bind:modalLogin="modalLogin"
+      v-on:changeModalLogin="changeModalLogin"
+      v-on:openModalLogin="openModalLogin"
+    />
+    <SignUp
+      v-if="!modalSigin && modalSignup"
+      v-bind:modalSignup="modalSignup"
+      v-on:changeModalLogin="changeModalLogin"
+      v-on:changeModalSignUp="changeModalSignUp"
+      v-on:openModalLogin="openModalLogin"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/stylesheets/useFree.scss";
+</style>
+
+<script>
+import SignUp from "./SignUp.vue";
+export default {
+  data() {
+    return { modalLogin: false, modalSignup: false, modalSigin: false };
+  },
+  components: { SignUp },
+  methods: {
+    changeModalSignUp: function () {
+      this.modalSignup = false;
+    },
+    openModalSignUp: function () {
+      this.modalSignup = true;
+    },
+    openModalLogin: function () {
+      this.modalLogin = true;
+    },
+    changeModalLogin: function () {
+      this.modalLogin = false;
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Header />
+    <Banner />
+    <Impactory />
+    <Competitor />
+    <CompExamples />
+    <UseFree />
+    <Footer />
+  </div>
 </template>
 
+<style lang="scss" scoped>
+@import "../assets/stylesheets/main.sass";
+@import "../assets/stylesheets/global.scss";
+</style>;
+
 <script>
-export default {}
+import Header from "../container/Header.vue";
+import Footer from "../container/Footer.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+
+    Footer,
+  },
+};
 </script>
